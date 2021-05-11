18f9f17aeaf28f260ff933f2517496e787749534
gianluca <gianlucachiap@gmail.com>
2021-05-11 13:13:58 +0200

    first

---
+++ example/index.tsx
@@ -0,0 +1,62 @@
+import 'react-app-polyfill/ie11';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { IntlProvider, useIntl } from '../.';
+
+const messages = {
+  it: require('./lang/it.json'),
+  en: require('./lang/en.json'),
+};
+
+const App = () => {
+  const locale = 'en';
+  return (
+    <IntlProvider
+      locale={locale}
+      defaultLocale="en"
+      messages={messages[locale] as Record<string, string>}
+    >
+      <TestApp />
+    </IntlProvider>
+  );
+};
+
+function TestApp() {
+  const { formatMessage } = useIntl();
+  return (
+    <div>
+      <div>{formatMessage({ id: 'test' })}</div>
+      <div>
+        {formatMessage(
+          { id: 'testWithVariable' },
+          {
+            variable: 'VARIABLE',
+          }
+        )}
+      </div>
+      <div>
+        {formatMessage(
+          { id: 'testWithElementVariable' },
+          {
+            variable: (
+              <span style={{ fontWeight: 'bold' }}>VARIABLE ELEMENT</span>
+            ),
+          }
+        )}
+      </div>
+      <div>
+        {formatMessage(
+          { id: 'testWithElementVariable' },
+          {
+            variable: (
+              <span style={{ color: 'red' }}>
+                {formatMessage({ id: 'test' })}
+              </span>
+            ),
+          }
+        )}
+      </div>
+    </div>
+  );
+}
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ src/index.tsx
@@ -0,0 +1,106 @@
+import React, { PropsWithChildren, useContext, useMemo } from 'react';
+
+interface IntlContext {
+  locale: string;
+  messages: Record<string, string | string[]>;
+  defaultLocale: string;
+  formatMessage(
+    id: { id: string },
+    variables?: { [key: string]: string | number }
+  ): string;
+  formatMessage(
+    id: { id: string },
+    variables?: { [key: string]: React.ReactNode }
+  ): JSX.Element[] | string;
+}
+
+interface IntlProviderProps {
+  locale: string;
+  defaultLocale: string;
+  messages: Record<string, string>;
+}
+
+const IntlContext = React.createContext<IntlContext | null>(null);
+
+export function IntlProvider({
+  children,
+  locale,
+  messages,
+  defaultLocale,
+}: PropsWithChildren<IntlProviderProps>) {
+  function formatMessage(
+    id: { id: string },
+    variables?: { [key: string]: string | number }
+  ): string;
+
+  function formatMessage(
+    id: { id: string },
+    variables?: { [key: string]: React.ReactNode }
+  ): JSX.Element[];
+
+  function formatMessage(
+    id: { id: string },
+    variables?: { [key: string]: React.ReactNode }
+  ): string | JSX.Element[] {
+    if (!messages[id.id]) {
+      console.error('missing locale data for ', id);
+      return id.id;
+    }
+    let translatedStringSplitted = messages[id.id]
+      .split(/({[^}]+})/g)
+      .filter(el => el);
+
+    let canBeStringified = true;
+    const translatedStringSplittedWithVariablesReplaced = translatedStringSplitted.map(
+      (part, idx) => {
+        const matches = part.match(/\{(.*?)\}/);
+        if (matches) {
+          const variableToReplace = variables?.[matches[1]];
+          if (!variableToReplace) {
+            return '';
+          }
+          if (
+            typeof variableToReplace === 'string' ||
+            typeof variableToReplace === 'number'
+          ) {
+            return variableToReplace;
+          }
+          canBeStringified = false;
+          return <React.Fragment key={idx}>{variableToReplace}</React.Fragment>;
+        }
+        return part;
+      }
+    );
+
+    if (canBeStringified) {
+      return translatedStringSplittedWithVariablesReplaced.join(' ');
+    }
+
+    return translatedStringSplittedWithVariablesReplaced as JSX.Element[];
+  }
+
+  const value = useMemo(
+    () => ({
+      locale,
+      messages,
+      defaultLocale,
+      formatMessage,
+    }),
+    [locale, messages]
+  );
+  return <IntlContext.Provider value={value}>{children}</IntlContext.Provider>;
+}
+
+export function useIntl() {
+  const value = useContext(IntlContext);
+  if (!value) {
+    throw new Error(`useIntl must be used inside a IntlProvider`);
+  }
+
+  return useMemo(
+    () => ({
+      ...value,
+    }),
+    [value]
+  );
+}
